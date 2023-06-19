d73c41ccca22aa4fc44c47ad9b67fa9612691df7
Will Weston <Williamweston81@gmail.com>
2023-06-19 11:10:11 -0500

    Weston(1) Add comments

--- arrays.rb
+++ arrays.rb
@@ -1,4 +1,17 @@
-fruits = ["Kiwi", "Strawberry", "Lychee", "Dragonfruit"]
+fruit = ["Kiwi", "Strawberry", "Lychee", "Dragonfruit"]
 fruit_count = [4, 15, 18, 1]
 fruit_price = [2.32, 2.05, 8.30, 5.91]
-fruit_expense = [False, False, True, True]
+fruit_expense = [false, false, true, true]
+
+# .pop method will remove the last element in the array IE "Dragonfruit"
+puts fruit.pop
+
+# .push method will add 1 or more elements to the array IE "20"
+puts fruit_count.push (20)
+
+# .shift method removes leading element IE "2.32"
+puts fruit_price.shift
+
+# .unshift method will add 1 or more elements to beginning of array IE Boolean Operator True
+puts fruit_expense.unshift (true)
+
